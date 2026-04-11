91e66531224f23cea7b0c26a0900707437d882de
sumin <dwckey5356@gmail.com>
2026-04-11 15:45:39 +0900

    add crypto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import * as contract from "./contract";
 import * as reader from "./sdk/reader";
 import * as writer from "./sdk/writer";
+import * as crypto from "./sdk/crypto";
 import * as constants from "./sdk/constants";
 import * as utils from "./sdk/utils";
 import { setRpcUrl, getRpcUrl } from "./sdk/utils/provider";
 
-export { contract, reader, writer, constants, utils, setRpcUrl, getRpcUrl };
+export { contract, reader, writer, crypto, constants, utils, setRpcUrl, getRpcUrl };
 
-const iqlabs = { contract, reader, writer, utils, constants, setRpcUrl, getRpcUrl };
+const iqlabs = { contract, reader, writer, crypto, utils, constants, setRpcUrl, getRpcUrl };
 export { iqlabs };
 export default iqlabs;
--- src/sdk/crypto/index.ts
+++ src/sdk/crypto/index.ts
@@ -1,24 +1,7 @@
-// =============================================================================
-//  Crypto Module (Solana SDK에서 100% 복사 — 체인 무관)
-// =============================================================================
-//
-//  TODO: iqlabs-solana-sdk/src/sdk/crypto/ 에서 전체 복사
-//
-//  이 모듈의 모든 함수는 Web Crypto API + @noble/curves만 사용.
-//  Solana/Ethereum 어느 체인에도 의존하지 않음.
-//
-//  파일 목록:
-//    primitives.ts — getRandomBytes, hkdfDerive, aesEncrypt, aesDecrypt, pbkdf2Derive
-//    encoding.ts   — hexToBytes, bytesToHex, validatePubKey
-//    dh.ts         — deriveX25519Keypair, dhEncrypt, dhDecrypt
-//    password.ts   — passwordEncrypt, passwordDecrypt
-//    multi.ts      — multiEncrypt, multiDecrypt
-//
-//  deriveX25519Keypair의 signMessage 인자는 ethers Signer.signMessage()과 호환됨:
-//    const signer = new ethers.Wallet(privateKey);
-//    const keypair = await deriveX25519Keypair((msg) => signer.signMessage(msg));
-
-// export { deriveX25519Keypair, dhEncrypt, dhDecrypt } from "./dh";
-// export { passwordEncrypt, passwordDecrypt } from "./password";
-// export { multiEncrypt, multiDecrypt } from "./multi";
-// export { hexToBytes, bytesToHex, validatePubKey } from "./encoding";
+export { hexToBytes, bytesToHex, validatePubKey } from "./encoding";
+export { deriveX25519Keypair, dhEncrypt, dhDecrypt } from "./dh";
+export { passwordEncrypt, passwordDecrypt } from "./password";
+export { multiEncrypt, multiDecrypt } from "./multi";
+export type { DhEncryptResult } from "./dh";
+export type { PasswordEncryptResult } from "./password";
+export type { MultiEncryptResult, RecipientEntry } from "./multi";
