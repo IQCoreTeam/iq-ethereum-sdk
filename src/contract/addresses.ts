8ace0a554555a9f15cebb7d415a1c4b72a7d55cc
sumin <dwckey5356@gmail.com>
2026-04-11 15:55:31 +0900

    edit it as only call data

--- src/contract/addresses.ts
+++ src/contract/addresses.ts
@@ -1,7 +1,7 @@
 import { Contract, type Provider, type Signer } from "ethers";
 import { CODEIN_ABI } from "./abi";
 
-export const DEFAULT_CONTRACT_ADDRESS = "0x905dABC7348905AC0Ba94776a24661be5a8cB2e1";
+export const DEFAULT_CONTRACT_ADDRESS = "0xb190FC39B100Bce7a3119Fb5b5836C5d02190C67";
 
 export function getContract(signerOrProvider: Signer | Provider, address = DEFAULT_CONTRACT_ADDRESS) {
   return new Contract(address, CODEIN_ABI, signerOrProvider);
